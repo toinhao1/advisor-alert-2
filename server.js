f12d9090f103f2ce7b5d058b2ae5987117dd8d8a
toinhao1 <toinhao89@gmail.com>
2019-06-05 13:23:05 +0200

    Putting into production

--- server.js
+++ server.js
@@ -41,6 +41,16 @@ app.use('/users', users);
 app.use('/clients', clients);
 app.use('/alerts', alerts);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Set port and have server listen
 const port = process.env.PORT || 5000;
 
